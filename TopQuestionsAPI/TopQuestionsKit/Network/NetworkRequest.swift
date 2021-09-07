6e0b8a1ddda6a6c68dd777c5419f7aabb6824615
Chuy <jesus.cruz@oracle.com>
2021-09-07 13:04:53 -0500

    Delete Network Request API

--- TopQuestionsAPI/TopQuestionsKit/Network/NetworkRequest.swift
+++ TopQuestionsAPI/TopQuestionsKit/Network/NetworkRequest.swift
@@ -9,77 +9,13 @@ import Foundation
 import UIKit
 
 // MARK: - NetworkRequest
-public protocol NetworkRequest: AnyObject {
-    associatedtype ModelType
-    func decode(_ data: Data) -> ModelType?
-    func execute(withCompletion completion: @escaping (ModelType?) -> Void)
-}
 
-public extension NetworkRequest {
-    fileprivate func load(_ url: URL, withCompletion completion: @escaping (ModelType?) -> Void) {
-        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, _ , _) -> Void in
-            guard let data = data, let value = self?.decode(data) else {
-                DispatchQueue.main.async { completion(nil) }
-                return
-            }
-            DispatchQueue.main.async { completion(value) }
-        }
-        task.resume()
-    }
-}
 
-// MARK: - ImageRequest
-public class ImageRequest {
-    let url: URL
-    
-    init(url: URL) {
-        self.url = url
-    }
-}
 
-extension ImageRequest: NetworkRequest {
-    public func decode(_ data: Data) -> UIImage? {
-        return UIImage(data: data)
-    }
-    
-    public func execute(withCompletion completion: @escaping (UIImage?) -> Void) {
-        load(url, withCompletion: completion)
-    }
-}
-
-// MARK: - APIRequest
-public class APIRequest<Resource: APIResource> {
-    let resource: Resource
-    
-    init(resource: Resource) {
-        self.resource = resource
-    }
-}
-
-extension APIRequest: NetworkRequest {
-    public func decode(_ data: Data) -> [Resource.ModelType]? {
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .secondsSince1970
-        let wrapper = try? decoder.decode(Wrapper<Resource.ModelType>.self, from: data)
-        return wrapper?.items
-    }
-    
-    public func execute(withCompletion completion: @escaping ([Resource.ModelType]?) -> Void) {
-        load(resource.url, withCompletion: completion)
-    }
-}
-
-// MARK: - APIResource
-public protocol APIResource {
-    associatedtype ModelType: Decodable
-    var methodPath: String { get }
-    var filter: String? { get }
-}
-
-public extension APIResource {
-    var url: URL {
-        var components = URLComponents(string: "https://api.stackexchange.com/2.2")!
-        components.path = methodPath
+// MARK: - APIURLResources
+struct APIURLResources {
+    static func getQuestionsURL() -> URL {
+        var components = URLComponents(string: "https://api.stackexchange.com/questions")!
         components.queryItems = [
             URLQueryItem(name: "site", value: "stackoverflow"),
             URLQueryItem(name: "order", value: "desc"),
@@ -87,26 +23,19 @@ public extension APIResource {
             URLQueryItem(name: "tagged", value: "swiftui"),
             URLQueryItem(name: "pagesize", value: "10")
         ]
-        if let filter = filter {
-            components.queryItems?.append(URLQueryItem(name: "filter", value: filter))
-        }
         return components.url!
     }
-}
-
-// MARK: - QuestionsResource
-public struct QuestionsResource: APIResource {
-    public typealias ModelType = Question
-    var id: Int?
     
-    public var methodPath: String {
-        guard let id = id else {
-            return "/questions"
-        }
-        return "/questions/\(id)"
-    }
-    
-    public var filter: String? {
-        id != nil ? "!9_bDDxJY5" : nil
+    static func getQuestionsDetailURL(id: Int) -> URL {
+        var components = URLComponents(string: "https://api.stackexchange.com/questions/\(id)")!
+        components.queryItems = [
+            URLQueryItem(name: "site", value: "stackoverflow"),
+            URLQueryItem(name: "order", value: "desc"),
+            URLQueryItem(name: "sort", value: "votes"),
+            URLQueryItem(name: "tagged", value: "swiftui"),
+            URLQueryItem(name: "pagesize", value: "10"),
+            URLQueryItem(name: "filter", value: "!9_bDDxJY5")
+        ]
+        return components.url!
     }
 }
